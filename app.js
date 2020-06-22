73fd635f07d3760f914a3b9cb4b4deaa549d6cc4
ashish1508 <belide.ashish@gmail.com>
2020-06-22 20:59:16 +0530

    cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const app=express()
-
+const cors=require('cors')
 const d_b=require('./db_firebase').d_b
 const bodyParser=require('body-parser')
 
@@ -75,7 +75,7 @@ app.use(bodyParser.json())
 // })
 
 
-
+app.use(cors())
 
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*')
