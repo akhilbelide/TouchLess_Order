34cfa0f804cf9974cc1142c2f70d71a00087ba1b
ashish1508 <belide.ashish@gmail.com>
2020-06-22 21:13:26 +0530

    worst_2

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*')
+    // res.setHeader('Access-Control-Allow-Origin','*')
     res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, PATCH')
     res.setHeader('Access-Control-Allow-Headers','Content-type, Authorization')
     next()
