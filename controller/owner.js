8cd6816046004165ee43535444d7f4174e94ebd0
ashish1508 <belide.ashish@gmail.com>
2020-06-22 19:27:08 +0530

    changing order status bug fixed_3

--- controller/owner.js
+++ controller/owner.js
@@ -18,11 +18,13 @@ exports.getOrders=(req,res,next)=>{
 
 exports.modifyOrder=(req,res,next)=>{
     const ord_id=req.body.order_id
+    console.log(ord_id)
     const modRef=d_b.ref('Orders').orderByChild('order_id').equalTo(ord_id)
-    modRef.once('child_added',snap =>{
-        snap.ref.update({
-            order_status:'DONE'
-        })
+    modRef.once('value',snap =>{
+        const k=Object.keys(snap.val())
+        snap.ref.child(`${k}`).update({
+             order_status:'DONE'
+         })
     }).then(result =>{
         res.status(200).json({message:'success!'})
     })
